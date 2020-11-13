6c31f143eb8e7e376818d5aff20130956be996e0
Thiago Leão <thiago.leao@mandarin.com.br>
2020-11-13 12:13:55 -0300

    ajuste final responsividade

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+var click = false;
+
+function switchMenu(){
+    click = !click;
+    var menuDisplay;
+
+    if(click){
+        menuDisplay = 'none'
+        imagem = '/assets/images/hamburguer.png'
+    }
+    else{
+        menuDisplay='flex'
+        imagem = '/assets/images/close.png'
+    }
+
+    document.querySelector(".menu-options-mobile").style.display = menuDisplay;
+    document.querySelector(".icon-responsive-menu").src = imagem;
+}
